15d1e9dfc77c826d3f3c9684c0bc8a51296f33bf
hellonaeunkim <hellonaeunkim@gmail.com>
2024-11-20 17:39:59 +0900

    work

--- 2024-11-18-wise-saying-app/src/main/java/com/ll/Main.java
+++ 2024-11-18-wise-saying-app/src/main/java/com/ll/Main.java
@@ -17,11 +17,16 @@ class App {
 
     while (true) {
       System.out.print("명령 ) ");
-
       String cmd = scanner.nextLine();
 
-      if (cmd.equals("종료")) break;
-
+      if (cmd.equals("종료")) {
+        break;
+      } else if (cmd.equals("등록")) {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+      }
     }
     scanner.close();
   }
