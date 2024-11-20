12bdc43bd34defdd1cdc8ceafe98d1a014c31fad
hellonaeunkim <hellonaeunkim@gmail.com>
2024-11-20 14:43:34 +0900

    Add/Update subfolder contents

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,32 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    App app = new App();
+    app.run();
+  }
+}
 
+class App {
+  public void run() {
+    System.out.println("== 명언 앱 ==");
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      System.out.print("명령 ) ");
+
+      String cmd = scanner.nextLine();
+
+      if(cmd.equals("종료")) {
+        break;
+      }else if (cmd.equals("등록")) {
+        System.out.print("명언 : ");
+      }
+
+    }
+    scanner.close();
   }
 }
