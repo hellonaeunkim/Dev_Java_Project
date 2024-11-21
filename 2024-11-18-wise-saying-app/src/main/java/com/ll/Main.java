475a83e2b637d9fe9216b5ce258d4bc61cc67552
hellonaeunkim <hellonaeunkim@gmail.com>
2024-11-22 00:36:41 +0900

    work

--- 2024-11-18-wise-saying-app/src/main/java/com/ll/Main.java
+++ 2024-11-18-wise-saying-app/src/main/java/com/ll/Main.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -16,6 +18,7 @@ class App {
     Scanner scanner = new Scanner(System.in);
 
     int lastID = 0;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
 
     while (true) {
       System.out.print("명령 ) ");
@@ -30,15 +33,20 @@ class App {
         String author = scanner.nextLine();
         int id = ++lastID;
 
-        WiseSaying wiseSaying = new WiseSaying();
-        wiseSaying.id = id;
-        wiseSaying.content = content;
-        wiseSaying.author = author;
+        // WiseSaying을 객체에 생성하고 ArrayList에 추가
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
 
         System.out.println( id + "번 명언이 등록되었습니다.");
       } else if (cmd.equals("목록")) {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("------------------------");
+
+        // 역순으로 출력 (마지막에 등록된 명언이 가장 먼저 출력됨)
+        for (int i = wiseSayings.size() -1; i >= 0; i--) {
+          WiseSaying ws = wiseSayings.get(i);
+          System.out.println(ws.id + " / " + ws.author + " / " + ws.content);
+        }
       }
     }
     scanner.close();
@@ -49,5 +57,11 @@ class WiseSaying {
   int id;
   String content;
   String author;
-}
 
+  // 생성자 추가
+  public WiseSaying(int id, String content, String author) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
